582096b26f50820486fb13c7d613f94ca914ea74
Prashant-mahajan <prashantmahajan70@gmail.com>
2016-11-23 23:43:44 +0530

    Update Tic Tac Toe.py
    Added three new functions

--- Tic Tac Toe.py
+++ Tic Tac Toe.py
@@ -14,3 +14,23 @@ def drawBoard(board):
 	print("" + board[1] + "|" + board[2] + "|" + board[3])
 	print("	|	|")
 
+def inputPlayerLetter():
+	letter = ""
+	while not (letter=='X' or letter == 'O'):
+		print("Do you want to be X or O?")
+		letter = raw_input.upper()
+	if letter == 'X':
+		return ['X','O']
+	else:
+		return['O','X']
+
+def whoGoesFirst():
+	if random.randint(0,1) == 0:
+		return 'computer'
+	else:
+		return 'player'
+		
+#function returns True if the player types in 'yes', 'YES', 'y', or anything that begins with the letter Y
+def playAgain():
+	print("Do you want to play again? (yes or no) ")
+	return input().lower().startswith('y')	
